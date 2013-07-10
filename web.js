b3829e6d584803e7777774b796199787fb8b1d42
sylviaatraxler <sylviaatraxler@gmail.com>
2013-07-10 06:51:41 -0700

    Web.js updated to use index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = new Buffer(256);
+  var fs = require('fs');
+  response.send(buff.toString(fs.readFileSync('index.html')));
+ // response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
